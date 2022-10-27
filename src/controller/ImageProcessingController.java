078bdbd22bcf13a5d9be1ef5c65e771041af7ced
Trang Do <116769130+pone-mole@users.noreply.github.com>
2022-10-27 19:28:42 -0400

    add new controller class and reformatted utils class; have not coded anything of substance yet

---
+++ src/controller/ImageProcessingController.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * This interface represents a controller for processing image to the designated intention of the
+ * user based on their input.
+ */
+public interface ImageProcessingController {
+  /**
+   * Start the image processing program.
+   */
+  void start();
+}
