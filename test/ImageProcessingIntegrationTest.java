e3fe9a80d334bc83f43c4fd2ac55983201133cd6
aulin1 <audrey.youran.lin@gmail.com>
2022-11-01 20:52:11 -0400

    Test commit
    Test commit - not finished

--- test/ImageProcessingIntegrationTest.java
+++ test/ImageProcessingIntegrationTest.java
@@ -82,58 +82,86 @@ public class ImageProcessingIntegrationTest {
     Map<String, ImageProcessingModel> map = new HashMap<>;
     ImageProcessingView test = new PPMProcessingView(map);
     ImageProcessingModel model = test.loadImage(filePath, fileName);
-    assertArrayEquals(model.getImage(), pixelArr);
+    assertArrayEquals(pixelArr, model.getImage());
     assertArrayEquals(model.getImage(), test.getModel(fileName).getImage());
+    assertArrayEquals(model.getImage(), map.get(fileName).getImage());
   }
 
   /**
    * Tests if saving an image works with the view.
-   *
+   * */
   @Test
   public void testSave(){
-    int[][][] img = {{{0, 0, 0}}};
-    ImageProcessingModel model = new PPMProcessingModel(img, 255,"dot.ppm");
     Map<String, ImageProcessingModel> map = new HashMap<>;
     ImageProcessingView test = new PPMProcessingView(map);
-    test.saveImage("dot.ppm", model);
-    ImageProcessingModel model2 = test.loadImage();
-    assertArrayEquals(model.getImage(),model2.getImage());
+    test.saveImage(filePath, fileName);
+    ImageProcessingModel model2 = test.loadImage(filePath, fileName);
+    assertArrayEquals(test.getModel(fileName).getImage(),model2.getImage());
+    assertArrayEquals(pixelArr, model2.getImage());
   }
 
   /**
    * Tests if storing an image works.
-   *
+   * */
   @Test
   public void testStoreImage(){
-    int[][][] img = {}
+    int[][][] img = {{{0, 0, 0}}};
+    ImageProcessingModel model = new PPMProcessingModel(img, 255,"dot.ppm");
+    Map<String, ImageProcessingModel> map = new HashMap<>;
+    ImageProcessingView test = new PPMProcessingView(map);
+    test.storeImage("dot.ppm", model);
+    assertArrayEquals(model.getImage(), test.getModel("dot.ppm").getImage());
+    assertArrayEquals(model.getImage(), map.get("dot.ppm").getImage());
   }
 
   /**
    * Tests if changing a name works.
-   *
+   **/
+  @Test
+  public void testChangeName(){
+    int[][][] img = {{{0, 0, 0}}};
+    ImageProcessingModel model = new PPMProcessingModel(img, 255,"dot.ppm");
+    Map<String, ImageProcessingModel> map = new HashMap<>;
+    ImageProcessingView test = new PPMProcessingView(map);
+    test.storeImage("dot.ppm", model);
+    test.changeName("dot.ppm", "name.ppm");
+    assertArrayEquals(model.getImage(), test.getModel("name.ppm").getImage());
+    assertArrayEquals(model.getImage(), map.get("name.ppm").getImage());
+    assertNull(map.get("dot.ppm"));
+  }
+
+  /**
+   * Tests each command individually.
+   * */
+  @Test
+  public void testAllCommands(){
+    
+  }
+
 
   /**
    * Tests if loading, using multiple correct commands works, and saving works.
-   *
+   * */
   @Test
   public void testCommands(){
     StringBuffer out = new StringBuffer();
     StringReader in = new StringReader("load " + filePath + " " + fileName + "red-component"
             + " horizontal-flip brighten 10 save res/PixelEdit.ppm PixelEdit.ppm q");
-    ImageProcessingController test = new ImageProcessingControllerImpl(out, in);
+    Map<String, ImageProcessingModel> map = new HashMap<>;
+    ImageProcessingView view = new PPMProcessingView(map);
+    ImageProcessingController test = new ImageProcessingControllerImpl(out, in, view);
     test.start();
-    ImageProcessingView view =
-            new PPMProcessingView("res/PixelEdit.ppm", "PixelEdit.ppm");
     assertArrayEquals(new int[][][] {{{137, 137, 137}, {163, 163, 163}, {163, 163, 163}, {10, 10, 10}},
             {{163, 163, 163}, {215, 215, 215}, {247,247,247}, {163, 163, 163}},
             {{163, 163, 163}, {255,255,255}, {22,22,22}, {163, 163, 163}},
             {{255,255,255}, {163, 163, 163}, {163, 163, 163}, {10,10,10}}},
-            view.loadImage().getImage());
+            view.getModel("PixelEdit.ppm").getImage());
+    //TODO: Fix once you know how the controller saves things.
   }
 
   /**
    * Tests if using an incorrect command does what is expected.
-   *
+   * */
   @Test
   public void testIncorrectCommand(){
     StringBuffer out = new StringBuffer();
@@ -149,7 +177,7 @@ public class ImageProcessingIntegrationTest {
 
   /**
    * Tests if using an incorrect, then correct command works.
-   *
+   * */
   @Test
   public void testIncorrectCorrectCommand(){
     StringBuffer out = new StringBuffer();
@@ -169,7 +197,7 @@ public class ImageProcessingIntegrationTest {
 
   /**
    * Tests if it throws an IllegalStateException if not quit.
-   *
+   * */
   @Test(expected = IllegalStateException.class)
   public void testNoQuit(){
     StringBuffer out = new StringBuffer();
@@ -181,7 +209,7 @@ public class ImageProcessingIntegrationTest {
 
   /**
    * Tests if loading in multiple images works.
-   *
+   * */
   @Test
   public void testLoadMultiple(){
     StringBuffer out = new StringBuffer();
@@ -189,6 +217,6 @@ public class ImageProcessingIntegrationTest {
             + "res/img.ppm img.ppm load res/Koala.ppm Koala.ppm");
     ImageProcessingController test = new ImageProcessingControllerImpl(out, in);
     test.start();
-    //add asserts once you know what's happening with controller
-  }*/
+    //TODO: add asserts once you know what's happening with controller
+  }
 }
